74df210252a327dde9dc846452dc4d782e217968
Mike <mikemcguiness@protonmail.com>
2023-03-14 18:34:24 -0700

    fix bug with images not appearing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/service-now-auto-repair' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
