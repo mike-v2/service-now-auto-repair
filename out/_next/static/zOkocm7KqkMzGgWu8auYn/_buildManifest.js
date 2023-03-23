8bae532a3a8de0a7b5c5badc155c26732c00e682
Mike <mikemcguiness@protonmail.com>
2023-03-22 18:54:25 -0700

    Deploy

--- out/_next/static/zOkocm7KqkMzGgWu8auYn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-a14a4ea6d2c0a12b.js","static/chunks/231-ccc23c26e8538ac6.js","static/chunks/pages/index-88c79d007ec22380.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
