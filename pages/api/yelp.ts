209386f5152e78ed196338e6a6e029b5dabd4c4f
Mike <mikemcguiness@protonmail.com>
2023-06-06 00:25:31 -0700

    improve error messages in Yelp API endpoint

--- pages/api/yelp.ts
+++ pages/api/yelp.ts
@@ -3,14 +3,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const url = 'https://api.yelp.com/v3/businesses/service-now-auto-repair-battleground';
+
     const response = await fetch(url,
       {
         headers: {
           Authorization: `Bearer ${process.env.YELP_API_KEY}`,
         }
       });
-    const data = await response.json();
-    res.status(200).json(data);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      res.status(200).json(data);
+    } else {
+      const errorData = await response.json();
+      console.error('Error fetching Yelp review score:', errorData);
+      res.status(response.status).json({ error: 'Error fetching Yelp review score' });
+    }
   } catch (error) {
     console.error('Error fetching review score:', error);
     res.status(500).json({ error: 'Error fetching review score' });
--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,6 @@ export default function Home() {
     }
   };
 
-  
   async function fetchYelpScore() {
     try {
       const response = await fetch('/api/yelp')
